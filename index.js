d9231cfd5a33fe956a4eeffdb040bc2e0e03468e
Morozova Maria <momary@yandex.ru>
2023-11-06 17:35:08 +0300

    After prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import lodash from "lodash";
 
-lodash.intersection([2,1],[2,3]);
+lodash.intersection([2, 1], [2, 3]);
 lodash.capitalize("hello");
 console.log("I love hexlet!");
-console.log(lodash.last(['one', 'two']));
+console.log(lodash.last(["one", "two"]));
